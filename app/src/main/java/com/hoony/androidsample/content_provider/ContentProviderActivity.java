88233645e225136aa717c283aa2a6ef3e1f4e831
TaeHoon <thkim9373@gmail.com>
2019-12-30 00:06:56 +0900

    Update a content provider activity using a cursor loader.

--- app/src/main/java/com/hoony/androidsample/content_provider/ContentProviderActivity.java
+++ app/src/main/java/com/hoony/androidsample/content_provider/ContentProviderActivity.java
@@ -1,12 +1,13 @@
 package com.hoony.androidsample.content_provider;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.Build;
+import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.provider.Settings;
@@ -16,51 +17,48 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
+import androidx.loader.content.Loader;
 
 import com.hoony.androidsample.R;
 import com.hoony.androidsample.databinding.ActivityContentProviderBinding;
 
-public class ContentProviderActivity extends AppCompatActivity {
+public class ContentProviderActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final int ADDRESS_BOOK_LOADER = "address_book_loader".hashCode();
 
     private final String[] PERMISSIONS = {Manifest.permission.READ_CONTACTS};
+
+    private CursorLoader loader;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(PERMISSIONS[0]) == PackageManager.PERMISSION_DENIED) {
-                requestPermissions(PERMISSIONS, 1);
-                return;
-            }
-        }
+        if (isDeniedPermissions()) return;
 
-        showContents();
+        loadData();
     }
 
-    private void showContents() {
-        ActivityContentProviderBinding binding = DataBindingUtil.setContentView(ContentProviderActivity.this, R.layout.activity_content_provider);
-
-        Cursor cursor = getContentResolver().query(
-                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                null,
-                null,
-                null,
-                null
-        );
-        if (cursor == null) return;
-
-        StringBuilder builder = new StringBuilder();
-        builder.append("================================\n\n");
-        while (cursor.moveToNext()) {
-            builder.append("Display name: ").append(cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))).append("\n")
-                    .append("Phone number: ").append(cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))).append("\n\n")
-                    .append("================================\n\n");
+    @TargetApi(VERSION_CODES.M)
+    private boolean isDeniedPermissions() {
+        if (checkSelfPermission(PERMISSIONS[0]) == PackageManager.PERMISSION_DENIED) {
+            requestPermissions(PERMISSIONS, 1);
+            return true;
         }
+        return false;
+    }
 
-        binding.tvAddressBookContents.setText(builder.toString());
+    private void loadData() {
+        loader = (CursorLoader) LoaderManager.getInstance(ContentProviderActivity.this).initLoader(ADDRESS_BOOK_LOADER, null, ContentProviderActivity.this);
+        loader.loadInBackground();
+    }
 
-        cursor.close();
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (loader != null) loader.abandon();
     }
 
     @Override
@@ -94,6 +92,52 @@ public class ContentProviderActivity extends AppCompatActivity {
             }
         }
 
-        showContents();
+        loadData();
+    }
+
+    @NonNull
+    @Override
+    public CursorLoader onCreateLoader(int id, @Nullable Bundle args) {
+        return new CursorLoader(ContentProviderActivity.this,
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                new String[]{
+                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                        ContactsContract.CommonDataKinds.Phone.NUMBER
+                },
+                null,
+                null,
+                null
+        );
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
+        showContents(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader loader) {
+        showContents(null);
+    }
+
+    private void showContents(Cursor cursor) {
+        ActivityContentProviderBinding binding = DataBindingUtil.setContentView(ContentProviderActivity.this, R.layout.activity_content_provider);
+
+        if (cursor == null) {
+            binding.tvAddressBookContents.setText("");
+        } else {
+
+            StringBuilder builder = new StringBuilder();
+            builder.append("================================\n\n");
+            while (cursor.moveToNext()) {
+                builder.append("Display name: ").append(cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))).append("\n")
+                        .append("Phone number: ").append(cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))).append("\n\n")
+                        .append("================================\n\n");
+            }
+
+            binding.tvAddressBookContents.setText(builder.toString());
+
+            cursor.close();
+        }
     }
 }
--- app/src/main/java/com/hoony/androidsample/photo_grid_view/PhotoGridViewActivity.java
+++ app/src/main/java/com/hoony/androidsample/photo_grid_view/PhotoGridViewActivity.java
@@ -1,6 +1,13 @@
 package com.hoony.androidsample.photo_grid_view;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PhotoGridViewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
