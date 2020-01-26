df1708dcdeab944ba940cb6d657e625d06bc86d1
TaeHoon <thkim9373@gmail.com>
2020-01-26 23:50:59 +0900

    Calculator example UI update(layout, not finish.)

--- app/src/main/java/com/hoony/androidsample/calculator/CalculatorActivity.java
+++ app/src/main/java/com/hoony/androidsample/calculator/CalculatorActivity.java
@@ -11,15 +11,21 @@ import com.hoony.androidsample.R;
 import com.hoony.androidsample.databinding.ActivityCalculatorBinding;
 
 public class CalculatorActivity extends AppCompatActivity implements View.OnClickListener {
+
     private ActivityCalculatorBinding binding;
-    private String a, b;
+    private String numericExpression;
+
+    private final String OPERATOR_ADDITION = " + ";
+    private final String OPERATOR_SUBTRACTION = " - ";
+    private final String OPERATOR_DIVISION = " * ";
+    private final String OPERATOR_MULTPLICATION = " / ";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = DataBindingUtil.setContentView(CalculatorActivity.this, R.layout.activity_calculator);
-        
+
         setListener();
     }
 
@@ -41,6 +47,34 @@ public class CalculatorActivity extends AppCompatActivity implements View.OnClic
 
     @Override
     public void onClick(View v) {
-
+        switch (v.getId()) {
+            case R.id.bt_1:
+                numericExpression += "1";
+                break;
+            case R.id.bt_2:
+                numericExpression += "2";
+                break;
+            case R.id.bt_3:
+                numericExpression += "3";
+                break;
+            case R.id.bt_4:
+                numericExpression += "4";
+                break;
+            case R.id.bt_5:
+                numericExpression += "5";
+                break;
+            case R.id.bt_6:
+                numericExpression += "6";
+                break;
+            case R.id.bt_7:
+                numericExpression += "7";
+                break;
+            case R.id.bt_8:
+                numericExpression += "8";
+                break;
+            case R.id.bt_9:
+                numericExpression += "9";
+                break;
+        }
     }
 }
